123a388eac2b230f305acd5fddbd65104bb618ea
umair ansari <umairnsr87@gmail.com>
2020-09-10 15:16:35 +0530

    queue with enqueue,dequeue

--- Queue.py
+++ Queue.py
@@ -2,13 +2,13 @@
 class Queue:
     def __init__(self):
         self.queue=list()
-    def inserttoqueue(self,value):
+    def enqueue(self,value):
         if value not in self.queue:
             self.queue.insert(0,value)
             print("Inserted element {} in queue".format(value))
             return True
         return False
-    def removefromqueue(self):
+    def dequeue(self):
         if len(self.queue)>0:
             self.queue.pop()
             print("Element deleted from queue and queue after deletion of element is {}".format(self.queue))
@@ -22,8 +22,8 @@ class Queue:
 queue=Queue()
 
 #inserty element into queue
-queue.inserttoqueue("Mon")
-queue.inserttoqueue("Trues")
-queue.inserttoqueue("wed")
+queue.enqueue("Mon")
+queue.enqueue("Trues")
+queue.enqueue("wed")
 #removing element from the queue
-queue.removefromqueue()
+queue.dequeue()
