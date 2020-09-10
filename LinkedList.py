04062346589a46d7a0b0c36fe64064c848db3529
umair ansari <umairnsr87@gmail.com>
2020-09-11 01:31:23 +0530

    Linked list traverse,insert at front

--- LinkedList.py
+++ LinkedList.py
@@ -15,6 +15,26 @@ class Linkedlist:
         newnode=Node(newdata)
         newnode.nextval=self.headval
         self.headval=newnode
+        print("Element Inserted")
+
+    def insertatend(self,newdata):
+        newnode=Node(newdata)
+        if self.headval is None:
+            self.headval=newnode
+            return
+        laste=self.headval
+        while(laste.nextval):
+            laste=laste.nextval
+        laste.nextval=newnode
+
+    def insertinbetween(self,middle_node,newdata):
+        if middle_node is None:
+            print("No such Node present")
+            return
+        newnode=Node(newdata)
+        newnode.nextval=middle_node.nextval
+        middle_node.nextval=newnode
+
 
 #creating the object of linkedlist and setting it to First created Node
 list1=Linkedlist()
@@ -29,11 +49,27 @@ list1.headval.nextval=e2
 #link 2nd node to third
 e2.nextval=e3
 
+#---------------------------------------------------
+print("Traversed linked list is")
 #traversion the linked list
 list1.listtraverse()
-print()
 
+
+#---------------------------------------------------
+print("Insertion at front")
 list1.insertatfront("Sun")
+#traversion the linked list
+list1.listtraverse()
 
+#---------------------------------------------------
+print("Insertion at the end")
+list1.insertatend("sat")
+#traversion the linked list
+list1.listtraverse()
+
+#---------------------------------------------------
+
+list1.insertinbetween(list1.headval.nextval,"Fri")
+print("The inbetween insertion leads to the list of")
 #traversion the linked list
 list1.listtraverse()
