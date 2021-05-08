b6b3ea87c78cf4de9ec1ce6bc8e5110b0a2f8077
databender <umairnsr87@gmail.com>
2021-05-09 03:16:21 +0530

    expression

--- 479A - expression.py
+++ 479A - expression.py
@@ -7,17 +7,13 @@ c=int(input())
 result=a*b*c
 
 if a*(b+c)>result:
-    # print("1")
     result=a*(b+c)
-    # print(result)
+
 if (a+b)*c>result:
-    # print("11")
     result=(a+b)*c
-    # print(result)
+
 if a+b+c>result:
-    # print("111")
     result=a+b+c
-    # print(result)
 
 
 print(result)
