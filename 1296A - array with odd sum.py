336ed0c7d310144a45fa176e47d572c18b7f5a6b
databender <umairnsr87@gmail.com>
2021-07-09 19:13:15 +0530

    array with odd sum

---
+++ 1296A - array with odd sum.py
@@ -0,0 +1,22 @@
+test = int(input())
+
+for _ in range(test):
+    n = int(input())
+    numbers = list(map(int,input().split()))
+    odd=0
+    even=0
+    for num in numbers:
+        if num%2!=0:
+            odd+=1
+        else:
+            even+=1
+
+    if (odd==n and n%2!=0 ) :
+        print("YES")
+    else:
+        print("NO")
+    
+    # if ((odd==n and n%2==0) or even==n):
+    #     print("NO")
+    # else:
+    #     print("YES")
